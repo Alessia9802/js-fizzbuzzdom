3caf53255cba860e8de350138e4a3974702e4d5d
Alessia9802 <alessiamisciagna@yahoo.it>
2021-10-13 17:10:38 +0200

    Struttura numeri da 1 a 100

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,12 +7,15 @@ per i multipli di 5
 e per i valori che sono sia multipli di 3 che di 5. */
 
 document.body.style.backgroundColor = "#183A4A";
+document.getElementById("titolo").style.color = "white";
 
 // Rappresentare un ciclo da 1 a 100
 
-const ul = document.querySelector("ul.list");
+let list = document.querySelector(".list");
 
 for (let i = 1; i <= 100; i++) {
   console.log(i);
-  ul.innerHTML += i;
+  const element = `<li class="box"> ${i} </li>`;
+
+  list.innerHTML += element;
 }
